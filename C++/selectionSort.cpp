da7a1766b0957fdb018fc568ca949c3f595d3960
prabhat7k <re311001boot@gmail.com>
2022-10-12 19:20:16 +0530

    Updated selectionSort

--- C++/selectionSort.cpp
+++ C++/selectionSort.cpp
@@ -9,16 +9,15 @@ void swap(int *x,int *y)
 }
 
 void selectionSort(int arr[], int n){
-	int i,j,min_index;
-	for(i=0;i<n-1;i++){
-		min_index=i;
-		for(j=i+1;j<n;j++){
-			if(arr[j]<arr[min_index])
-			min_index=j;
-			
-			swap(arr[i], arr[min_index]);
-		}
-	}
+    int i, j, min_idx;
+    for (i = 0; i < n-1; i++){
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+        if (arr[j] < arr[min_idx])
+            min_idx = j;
+        if(min_idx!=i)
+            swap(&arr[min_idx], &arr[i]);
+    }
 }
 int main(){
 	int n;
